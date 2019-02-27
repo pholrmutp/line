7e81a88f0e403a3ead15c8f1e75e8440535ada24
pholrmutp <48017689+pholrmutp@users.noreply.github.com>
2019-02-27 09:08:41 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = '+yvfpllsF0x8JZDsNrF+PlMOsXi4rj/SU5TIpVfBuXebs9C7hrEiEoc+Ws9V+W/wZsCyNAZNGkH54W9lpTUZ81H1N/90TRmIama6KDuq682PGdN0gwKgc0BaeoyEPBZ8xh4XJtkZ0RySUdhuMWAFKwdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'U3fc5b043ff752d5b78c9b2f5eb093084';
+$userId = 'Ue4fcf5fa0e1fa60d84433c7614e61b56';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
