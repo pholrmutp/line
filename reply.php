c4af07464f853a15b1139544ad6d30c3e9e9824f
pholrmutp <48017689+pholrmutp@users.noreply.github.com>
2019-02-27 08:47:53 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,5 +1,5 @@
 <?php
-require "vendor/autoload.php";
+require 'vendor/autoload.php';
 $access_token = '+yvfpllsF0x8JZDsNrF+PlMOsXi4rj/SU5TIpVfBuXebs9C7hrEiEoc+Ws9V+W/wZsCyNAZNGkH54W9lpTUZ81H1N/90TRmIama6KDuq682PGdN0gwKgc0BaeoyEPBZ8xh4XJtkZ0RySUdhuMWAFKwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '369c8c5d03cea66d8d167ed377f61598';
 $idPush = 'U3fc5b043ff752d5b78c9b2f5eb093084';
