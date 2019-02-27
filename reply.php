7dcf5c5e9170053af7d963c8fae9146c60745ded
pholrmutp <48017689+pholrmutp@users.noreply.github.com>
2019-02-27 08:36:55 +0700

    Create reply.php

---
+++ reply.php
@@ -0,0 +1,12 @@
+<?php
+require "vendor/autoload.php";
+$access_token = '+yvfpllsF0x8JZDsNrF+PlMOsXi4rj/SU5TIpVfBuXebs9C7hrEiEoc+Ws9V+W/wZsCyNAZNGkH54W9lpTUZ81H1N/90TRmIama6KDuq682PGdN0gwKgc0BaeoyEPBZ8xh4XJtkZ0RySUdhuMWAFKwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '369c8c5d03cea66d8d167ed377f61598';
+$idPush = 'U3fc5b043ff752d5b78c9b2f5eb093084'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+?>
